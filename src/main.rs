fc7958226643fcade15de6549fd1563bcf3e4cf4
Deadbeef <ent3rm4n@gmail.com>
2023-02-19 23:11:45 +0800

    script is after the table

--- src/main.rs
+++ src/main.rs
@@ -60,25 +60,6 @@ fn main() -> std::io::Result<()> {
                         border: 1px solid black;
                     }"
                 }
-                script {
-                    (PreEscaped(
-                    "
-                    // Copied Verbatim from https://stackoverflow.com/a/49041392.
-                    const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
-    
-                    const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
-                        v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-                        )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
-    
-                    // do the work...
-                    document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-                        const table = th.closest('table');
-                        Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
-                            .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-                            .forEach(tr => table.appendChild(tr) );
-                    })));
-                    "))
-                }
             }
             body {
                 table {
@@ -143,6 +124,25 @@ fn main() -> std::io::Result<()> {
                             }
                         )
                     })))
+                }
+                script {
+                    (PreEscaped(
+                    "
+                    // Copied Verbatim from https://stackoverflow.com/a/49041392.
+                    const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+    
+                    const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+                        v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+                        )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+    
+                    // do the work...
+                    document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+                        const table = th.closest('table');
+                        Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+                            .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+                            .forEach(tr => table.appendChild(tr) );
+                    })));
+                    "))
                 }
             }
         }
